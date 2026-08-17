fe39e5737301727e13d49fd0cddef9469eabd1e7
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-08-17 16:47:38 -0300

    feat: adição de exercícios

--- aluno/gabriel t/dom/tema.js
+++ aluno/gabriel t/dom/tema.js
@@ -1,3 +1,13 @@
-const fundoPagina = document.querySelector("body")
-
+const fundoPagina = document.querySelector("body");
 fundoPagina.style.backgroundColor = 'red';
+
+const titulos = document.querySelectorAll('h2');
+
+titulos.forEach(h2 => {
+    h2.style.fontFamily = "Arial, sans-serif"; 
+    h2.style.color = "white";
+});
+
+const metas = document.querySelector(".meta");
+
+metas.remove();
